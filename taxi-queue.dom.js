a22111f5de2956388a5cd1907b4ecdb823bb53de
Kabelo Madimabe <madimabekabelo@gmail.com>
2023-05-25 12:47:22 +0200

    First commit, join & leave buttons for passengers working

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -3,9 +3,27 @@
 
 // DOM element references
 
+const passengerQueCount = document.querySelector(".passenger_queue_count");
+const joinButton = document.querySelector(".join_queue");
+const leaveButton = document.querySelector(".leave_queue"); 
+const taxiQueCount = document.querySelector(".taxi_queue_count");
+const joinTaxiQue = document.querySelector(".join_taxi_queue"); 
+const departButton = document.querySelector(".depart")
+
 // create Factory Function instance
 
 const taxiQueue = TaxiQueue();
 
 // DOM events
 
+joinButton.addEventListener("click", function(){
+    passengerQueCount.innerHTML = Number(passengerQueCount.innerHTML) + 1
+    
+
+});
+
+leaveButton.addEventListener("click", function(){
+    passengerQueCount.innerHTML = Number(passengerQueCount.innerHTML) - 1
+
+
+});
